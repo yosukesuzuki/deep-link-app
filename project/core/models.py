5b557fe66fab24b6daf2300d7681a7d274301bbb
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2015-05-05 22:05:12 +0900

    add models.py

--- project/core/models.py
+++ project/core/models.py
@@ -13,3 +13,15 @@ class Article(db.Model):
 
     def __unicode__(self):
         return self.title
+
+class ShortURL(db.Model):
+    long_url = db.StringProperty(verbose_name='long URL', required=True)
+    fallback_url = db.StringProperty(verbose_name='Fallback URL')
+    ipad_url = db.StringProperty(verbose_name='iPad URL')
+    android_url = db.StringProperty(verbose_name='Android URL')
+    wp_url = db.StringProperty(verbose_name='WindowsPhone URL')
+    firefox_url = db.StringProperty(verbose_name='FireFox URL')
+    updated_at = db.DateTimeProperty(auto_now=True)
+    created_at = db.DateTimeProperty(auto_now_add=True)
+
+
